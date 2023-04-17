d8a3e4cfa18907aeb9b39ccff11cfb569766b75a
Juliana Castillo <jcastilloaraujo@icloud.com>
2023-04-17 15:34:54 -0600

    Prototipo water.ai

---
+++ app/src/main/java/com/example/talentland/Consulta.java
@@ -0,0 +1,14 @@
+package com.example.talentland;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Consulta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consulta);
+    }
+}
